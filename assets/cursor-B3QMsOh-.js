46bddbff64dfc93027f39aa86d59138cc917e57e
Zach Kelling <z@zeekay.io>
2026-06-24 23:45:42 -0700

    rebrand: replace Pars bundle with Lux-branded governance build
    lux.vote was serving a Pars-branded governance SPA (PARS/vePARS/MIGA tokens,
    PARS Vote wordmark, Pars ecosystem footer). Rebuilt the Fractal governance
    app with Lux branding (Lux DAO, LUX/veLUX/sLUX, Lux purple theme, lux.network
    URLs, Lux Improvement Proposals). Bundle now carries zero Pars tokens.
    
    Built from Lux-branded source; main bundle assets/index-DmvWkLg7.js.

---
+++ assets/cursor-B3QMsOh-.js
@@ -0,0 +1,4 @@
+import{b as o}from"./core-jx6Rcr0-.js";import"./index-DmvWkLg7.js";import"./index.es-7K2577rI.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
+//# sourceMappingURL=cursor-B3QMsOh-.js.map
---
+++ assets/filters-DIFbG-En.js
@@ -0,0 +1,9 @@
+import{b as o}from"./core-jx6Rcr0-.js";import"./index-DmvWkLg7.js";import"./index.es-7K2577rI.js";const a=o`<svg fill="none" viewBox="0 0 16 16">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M0 3a1 1 0 0 1 1-1h14a1 1 0 1 1 0 2H1a1 1 0 0 1-1-1Zm2.63 5.25a1 1 0 0 1 1-1h8.75a1 1 0 1 1 0 2H3.63a1 1 0 0 1-1-1Zm2.62 5.25a1 1 0 0 1 1-1h3.5a1 1 0 0 1 0 2h-3.5a1 1 0 0 1-1-1Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{a as filtersSvg};
+//# sourceMappingURL=filters-DIFbG-En.js.map
